c05452eab3966305eecce88a61cb5180231bbf4a
xtc2016xtc <728611827@qq.com>
2025-01-06 12:37:19 +0800

    初始化

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import {Button} from "@/components/ui/button.tsx";
+
+
+const App = () => {
+    return  (
+        <div>
+            <Button title="首页"/>
+        </div>
+    )
+}
+
+export default App
