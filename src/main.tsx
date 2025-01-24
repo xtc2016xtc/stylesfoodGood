2b9c77d81dc84aa0dafb1e6e22f6681d5834f0e8
xtc2016xtc <728611827@qq.com>
2025-01-24 18:08:07 +0800

    更新备注

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+const charAt = `
+    ███████╗██╗     ███████╗██╗   ██╗██╗███████╗
+    ██╔════╝██║     ██╔════╝██║   ██║██║██╔════╝
+    █████╗  ██║     █████╗  ██║   ██║██║███████╗
+    ██╔══╝  ██║     ██╔══╝  ╚██╗ ██╔╝██║╚════██║
+    ███████╗███████╗███████╗ ╚████╔╝ ██║███████║
+    ╚══════╝╚══════╝╚══════╝  ╚═══╝  ╚═╝╚══════╝
+`;
+console.info(`%c${charAt}`, "color: #5BE49B");
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
