7b042f7b6249891fdfe9987950de1d25de9d613f
xtc2016xtc <728611827@qq.com>
2025-01-25 18:37:04 +0800

    更新备注

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+/*备注*/
 const charAt = `
     ███████╗██╗     ███████╗██╗   ██╗██╗███████╗
     ██╔════╝██║     ██╔════╝██║   ██║██║██╔════╝
